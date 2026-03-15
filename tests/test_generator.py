846c1aa39740567f0f2a5a55d1fb36cef87556df
Niladri Das <bniladridas@users.noreply.github.com>
2026-03-15 10:01:12 +0530

    test: cover generator defaults

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -4,3 +4,8 @@ from userstring.generator import build_user_agent
 def test_user_agent():
     ua = build_user_agent("testapp", "1.0", "linux")
     assert ua == "testapp/1.0 (linux)"
+
+
+def test_user_agent_defaults():
+    ua = build_user_agent()
+    assert ua == "userstring/0.0.1 (unknown)"
